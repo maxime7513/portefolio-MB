c5308f6a4ef3617deba53464ac625b488282c1ae
maxime <maxbln7513@gmail.com>
2021-06-06 15:55:12 +0200

    adresse site autoplusservice37

--- php/section-realisation.php
+++ php/section-realisation.php
@@ -4,13 +4,13 @@
         <div class="d--flex flex--col jc--ce explication">
             <h4 class="text--center">Auto Plus Service</h4>
             <p class="text--center mb--3rem">Concessionaire automobile,<br>Vente de véhicules d'occasions et préstations mécanique</p>
-            <a href="https://autoplusservice.fr" class="m--auto"><button>Voir le site</button></a>
+            <a href="https://www.autoplusservice37.fr" class="m--auto"><button>Voir le site</button></a>
         </div>
         <div class="container-slider_realisation">
             <img class="slider_realisation1" src="./assets/img/site-AutoPlusService.png" alt="image site Auto Plus Service">
         </div>
     </div>
-    <div class="d--flex-rev flex--row flex--sg jc--sa ai--ce mt--2rem">
+    <div class="d--flex flex--row flex--sg jc--sa ai--ce mt--2rem">
         <div class="container-slider_realisation">
             <img class="slider_realisation2" src="./assets/img/site-MeetInMarseille.png" alt="image site MeetInMarseille">
         </div>
